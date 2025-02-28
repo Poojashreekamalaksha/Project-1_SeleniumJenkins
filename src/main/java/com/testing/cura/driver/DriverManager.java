9907d554f1e9a70963e589caa61efa7563490039
Poojashreekamalaksha <poojakamalaksha98@gmail.com>
2025-02-28 17:46:07 +0530

    initial commit 25/02/2025

--- src/main/java/com/testing/cura/driver/DriverManager.java
+++ src/main/java/com/testing/cura/driver/DriverManager.java
@@ -3,8 +3,7 @@ package com.testing.cura.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+
 
 public class DriverManager {
 
